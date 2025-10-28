28b03462ca77f2a06e3de03ae862bc1ac5053efb
Dan-Flores <danielflores3@meta.com>
2025-10-28 09:37:53 -0400

    Add copyright header to 7 files in /packaging (#999)
    Co-authored-by: Dan-Flores <danielflores3@fb.com>

--- packaging/build_ffmpeg.sh
+++ packaging/build_ffmpeg.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 # This is taken and adapated from torchaudio, only keeping the parts relevant to
 # linux.
